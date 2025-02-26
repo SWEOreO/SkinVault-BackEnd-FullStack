a756463b8d22e9cc31025d9bbe1c1169d96202d6
Kay Hsu <kayhsu@Kays-MacBook-Pro.local>
2025-02-26 10:09:57 -0800

    updated server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const {authenticateUser, logInWithToken, registerUser} = require('./db/users.js');
+const {registerUser, logInUser} = require('./db/users.js');
 const{fetchAllProducts, fetchProductDetails} = require('./db/products.js');
 const{fetchAllReviews, createReviews, deleteReview, getMyReviews} = require('./db/reviews.js');
 
@@ -11,6 +11,7 @@ app.use(express.static('dist'));
 const bcrypt = require('bcrypt');
 const client = require('./db/client.js');
 client.connect();
+
 require('dotenv').config();
 
 // // test
@@ -21,21 +22,25 @@ require('dotenv').config();
 // register
 app.post('/api/auth/register',async(req, res, next) => {
   const{username, password} = req.body;
-try{
-  await registerUser();
-} catch(err) {
-    next(err);}
+  try {
+    const regUser = await registerUser(username, password);
+    console.log(regUser);
+    res.send(regUser);
+  } catch (err) {
+    res.send({message: `Bad Credentials`});
+  }
 });
 
 
 // login
 app.post('/api/auth/login',async(req, res, next) => {
   const{username, password} = req.body;
-  try{
-    const token = await authenticateUser(username, password);
-    res.send({token: token});
-  } catch(err) {
-    res.send({message: 'Bad Credentials'});
+  try {
+    const user = await logInUser(username, password);
+    console.log(user);
+    res.send({user})
+  } catch (err) {
+    res.send({message: err.message});
   }
 });
 
@@ -43,7 +48,7 @@ app.post('/api/auth/login',async(req, res, next) => {
 // user info
 app.get('/api/auth/me',async(req, res, next) => {
   try{
-    const user = await logInWithToken(req.header.authenticateUser);
+    const user = await registerUser(req.headers.logInUser);
     res.send({user})
   } catch(err) {
     res.send({message: err.message});
@@ -55,6 +60,7 @@ app.get('/api/auth/me',async(req, res, next) => {
 app.get('/api/products', async(req, res, next) => {
   try{
     const allProducts = await fetchAllProducts();
+    res.send(allProducts);
   } catch(err) {
     next(err);
   }
@@ -63,63 +69,66 @@ app.get('/api/products', async(req, res, next) => {
 
 // certain product details
 app.get('/api/products/:product_id', async(req, res, next) => {
+  const {product_id} = req.params;
   try{
-    const productDetails = await fetchProductDetails();
-  } catch(err) {
-    next(err);
-  }
-});
-
-// matched review for certain product
-app.get('/api/product/:product_id/reviews', async(req, res, next) => {
-  try{
-    const allReviews = await fetchAllReviews();
-  } catch(err) {
-    next(err);
-  }
-});
-
-// create new reviews for cetain product(member only)
-app.post('/api/products/:productId/write-new-reviews', async(req, res, next) => {
-  const {productId} = req.params;
-  console.log(productId);
-  // console.log(id);
-  const{score,text, user_id, product_id} = req.body;
-
-  try{
-    const newReview = await createReviews(score, text, user_id, product_id);
-  }catch(err) {
-    next(err);
-  }
-  res.status(201).send(newReview);
-});
-
-// delete existing review for certain product (member only)
-app.delete('/api/reviews/:reviewId', async(req, res, next) => {
-  const {reviewId} = req.params;
-  console.log(reviewId);
-  try{
-
-    await deleteReview(reviewId);
-
-    res.status(204).send({});
+    const productDetails = await fetchProductDetails(product_id);
+    res.send(productDetails);
   } catch(err) {
     next(err);
   }
 });
 
 
-// check my reviews (members only)
-app.get('/api/review/me', async(req, res, next) => {
-  try{
+// // matched review for certain product
+// app.get('/api/product/:product_id/reviews', async(req, res, next) => {
+//   try{
+//     const allReviews = await fetchAllReviews();
+//   } catch(err) {
+//     next(err);
+//   }
+// });
 
-    await getMyReviews(user_id);
+// // create new reviews for cetain product(member only)
+// app.post('/api/products/:productId/write-new-reviews', async(req, res, next) => {
+//   const {productId} = req.params;
+//   console.log(productId);
+//   // console.log(id);
+//   const{score,text, user_id, product_id} = req.body;
 
-    res.status(204).send({});
-  } catch(err) {
-    next(err);
-  }
-});
+//   try{
+//     const newReview = await createReviews(score, text, user_id, product_id);
+//   }catch(err) {
+//     next(err);
+//   }
+//   res.status(201).send(newReview);
+// });
+
+// // delete existing review for certain product (member only)
+// app.delete('/api/reviews/:reviewId', async(req, res, next) => {
+//   const {reviewId} = req.params;
+//   console.log(reviewId);
+//   try{
+
+//     await deleteReview(reviewId);
+
+//     res.status(204).send({});
+//   } catch(err) {
+//     next(err);
+//   }
+// });
+
+
+// // check my reviews (members only)
+// app.get('/api/review/me', async(req, res, next) => {
+//   try{
+
+//     await getMyReviews(user_id);
+
+//     res.status(204).send({});
+//   } catch(err) {
+//     next(err);
+//   }
+// });
 
 
 const PORT = process.env.PORT || 3000;
